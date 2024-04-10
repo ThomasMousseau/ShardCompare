4f076879fd3f9849acece5292c289885fa7fb480
ThomasMousseau <univers220@gmail.com>
2024-04-10 17:36:27 -0400

    updated working bash script

---
+++ DeployNoSQL/deploy_mongo.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Configuration
+networkName="mongo-cluster"
+replicaSetName="rs0"
+mongoImage="mongo:4.4" 
+numNodes=3  #! A changer de 3 à 5
+current_iteration=1
+MAX_ITERATION=10
+host_data_dir="/home/thomasm/School/Poly-session-6/LOG8430/DeployNoSQL/current_data"
+container_data_dir="/usr/src/ycsb/data"
+
+
+deploy_mongo() {
+    docker network create ${networkName}
+
+    # Démarrage des conteneurs MongoDB
+    port=27017 
+    for i in $(seq 1 ${numNodes}); do
+        docker run -d --name "mongo${i}" -p ${port}:27017 --net ${networkName} \
+            ${mongoImage} mongod --replSet ${replicaSetName} --bind_ip_all
+        port=$((port+1))
+    done
+
+    echo "Attente de 5 secondes pour que les conteneurs soient prêts..."
+    sleep 5
+
+    # Construction de l'URI de connexion à MongoDB
+    # port=27017 
+    # mongodb_uri="mongodb://"
+    # for i in $(seq 1 ${numNodes}); do
+    #     mongodb_uri+="mongo${i}:27017,"
+    #     port=$((port+1))
+    # done
+    # mongodb_uri=${mongodb_uri%,}/ycsb?replicaSet=${replicaSetName}"&w=1"
+
+    docker exec -ti mongo1 mongo --eval "rs.initiate({
+      _id: '${replicaSetName}',
+      members: [
+        $(for i in $(seq 1 ${numNodes}); do echo "{ _id: $((i-1)), host: 'mongo${i}:27017' },"; done | sed '$s/,$//')
+      ]
+    })"
+    echo "L'ensemble de réplicas MongoDB a été initialisé."
+}
+
+run_ycsb() {
+
+    mongodb_uri="mongodb://mongo1:27017/ycsb?replicaSet=rs0&w=1"
+    #mongodb_uri="mongodb://mongo1:27017/ycsb?w=1" #!they all work
+    #mongodb_uri="mongodb://mongo1:27017,mongo2:27017,mongo3:27017/ycsb?replicaSet=rs0&w=1" #!they all work
+
+    # ./bin/ycsb.sh load mongodb -s -P workloads/workload_10_90 -p \"mongodb.url=${mongodb_uri}\" > data/load_10_90_${current_iteration}.txt;
+    # ./bin/ycsb.sh run mongodb -s -P workloads/workload_10_90 -p \"mongodb.url=${mongodb_uri}\" > data/run_10_90_${current_iteration}.txt;"
+
+    current_data_dir="${host_data_dir}/${current_iteration}"
+
+    mkdir -p ${current_data_dir}
+
+    docker run -d --name "ycsb${current_iteration}" --network ${networkName} -v "${current_data_dir}:/usr/src/ycsb/data" thomasmousseau/ycsb-0.17.0:latest /bin/sh -c "
+
+    mkdir data; 
+
+    ./bin/ycsb.sh load mongodb -s -P workloads/workloada -p \"mongodb.url=${mongodb_uri}\" > data/load_50_50_${current_iteration}.txt;
+    ./bin/ycsb.sh run mongodb -s -P workloads/workloada -p \"mongodb.url=${mongodb_uri}\" > data/run_50_50_${current_iteration}.txt;"
+    
+    docker wait "ycsb${current_iteration}"
+
+    #tail -f /dev/null"
+}
+
+delete_cluster() {
+    docker rm -f $(docker ps -a -q --filter="name=mongo")
+    docker rm -f $(docker ps -a -q --filter="name=ycsb")
+    docker network rm ${networkName}
+}
+
+for current_iteration in $(seq 1 ${MAX_ITERATION}); do
+    delete_cluster
+    deploy_mongo
+    run_ycsb
+done
+
+echo "Fin du script"
---
+++ DeployNoSQL/deploy_redis.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Configuration
+networkName="redis-replication"
+redisImage="redis:6.2.5" #redis:7.2.3
+num_slave_nodes=2  
+current_iteration=1
+host_data_dir="/home/thomasm/School/Poly-session-6/LOG8430/DeployNoSQL/current_data"
+MAX_ITERATION=10
+
+name=("redis-master" "redis-slave1" "redis-slave2")
+
+deploy_redis() {
+
+    docker network create ${networkName}
+    
+    #Start Redis containers
+    docker run -d \
+    --name ${name[0]} \
+    --network $networkName \
+    -p 6379:6379 \
+    $redisImage \
+    redis-server --appendonly yes --bind 0.0.0.0
+
+    for i in $(seq 1 $num_slave_nodes); do
+    #echo "Starting Redis slave $i && ${name[i]}"
+    docker run -d --name redis-slave${i} --network ${networkName} ${redisImage} \
+        redis-server --slaveof redis-master 6379 --appendonly yes --bind 0.0.0.0
+    done
+
+}
+
+run_ycsb() {
+
+    current_data_dir="${host_data_dir}/${current_iteration}"
+
+    mkdir -p ${current_data_dir}
+
+    #./bin/ycsb.sh load redis -s -P workloads/workloada -p "redis.host=172.29.0.2" -p "redis.port=6379"
+
+    # ./bin/ycsb.sh load redis -s -P workloads/workloada -p \"redis.host=${redis_master_ip}\" -p \"redis.port=6379\" > data/load_50_50_${current_iteration}.txt;
+    # ./bin/ycsb.sh run redis -s -P workloads/workloada -p \"redis.host=${redis_master_ip}\" -p \"redis.port=6379\" > data/run_50_50_${current_iteration}.txt;"
+
+    #redis-master
+    redis_master_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "redis-master")
+    echo "Redis master IP: ${redis_master_ip}"
+
+    docker run -d --name "ycsb${current_iteration}" --network ${networkName} -v "${current_data_dir}:/usr/src/ycsb/data" thomasmousseau/ycsb-0.17.0:latest /bin/sh -c "
+
+    mkdir data; 
+
+    ./bin/ycsb.sh load redis -s -P workloads/workload_10_90 -p \"redis.host=${redis_master_ip}\" -p \"redis.port=6379\" > data/load_10_90_${current_iteration}.txt;
+    ./bin/ycsb.sh run redis -s -P workloads/workload_10_90 -p \"redis.host=${redis_master_ip}\" -p \"redis.port=6379\" > data/run_10_90_${current_iteration}.txt;"
+    docker wait "ycsb${current_iteration}"
+
+    #tail -f /dev/null"
+
+}
+
+delete_cluster() {
+    docker rm -f $(docker ps -a -q --filter="name=redis")
+    docker rm -f $(docker ps -a -q --filter="name=ycsb")
+    docker network rm ${networkName}
+}
+
+for current_iteration in $(seq 1 ${MAX_ITERATION}); do
+    delete_cluster
+    deploy_redis
+    run_ycsb 
+done
+
+echo "End of script"
